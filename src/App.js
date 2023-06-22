6c06d4052e10ceeb33a41c26fd1e3b7ac4f798b6
Anshuman <anshuman.1207738@lpu.in>
2023-06-22 20:09:51 +0530

    Initial Commit!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import LoadingBar from "react-top-loading-bar";
 
-function App() {
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+  const pageSize = 6;
+  const apiKey = `8dd21e376f564d5a8d1b1a43ac8a7ed5`;
+  const [progress, setProgress] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <br /><br /><br /><br />
+      <Navbar />
+      <div>
+        <LoadingBar color="blue" height={5} progress={progress} />
+      </div>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <News
+              apiKey={apiKey}
+              setProgress={setProgress}
+              key="general"
+              pageSize={pageSize}
+              country="in"
+              category="general"
+            />
+          }
+        />
+        <Route
+          path="/business"
+          element={
+            <News
+              apiKey={apiKey}
+              setProgress={setProgress}
+              key="business"
+              pageSize={pageSize}
+              country="in"
+              category="business"
+            />
+          }
+        />
+        <Route
+          path="/entertainment"
+          element={
+            <News
+              apiKey={apiKey}
+              setProgress={setProgress}
+              key="entertainment"
+              pageSize={pageSize}
+              country="in"
+              category="entertainment"
+            />
+          }
+        />
+        <Route
+          path="/general"
+          element={
+            <News
+              apiKey={apiKey}
+              setProgress={setProgress}
+              key="general"
+              pageSize={pageSize}
+              country="in"
+              category="general"
+            />
+          }
+        />
+        <Route
+          path="/health"
+          element={
+            <News
+              apiKey={apiKey}
+              setProgress={setProgress}
+              key="health"
+              pageSize={pageSize}
+              country="in"
+              category="health"
+            />
+          }
+        />
+        <Route
+          path="/science"
+          element={
+            <News
+              apiKey={apiKey}
+              setProgress={setProgress}
+              key="science"
+              pageSize={pageSize}
+              country="in"
+              category="science"
+            />
+          }
+        />
+        <Route
+          path="/sports"
+          element={
+            <News
+              apiKey={apiKey}
+              setProgress={setProgress}
+              key="sports"
+              pageSize={pageSize}
+              country="in"
+              category="sports"
+            />
+          }
+        />
+        <Route
+          path="/technology"
+          element={
+            <News
+              apiKey={apiKey}
+              setProgress={setProgress}
+              key="technology"
+              pageSize={pageSize}
+              country="in"
+              category="technology"
+            />
+          }
+        />
+      </Routes>
     </div>
   );
-}
-
+};
 export default App;
---
+++ src/News.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function News(props) {
+    const category=props.categoryl;
+    const fetchNews=async()=>{
+        const url=fetch('')
+    }
+  return (
+    <div>
+      
+    </div>
+  )
+}
